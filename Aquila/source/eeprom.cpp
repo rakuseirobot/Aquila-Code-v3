492bfd763e51272ea2f95b7285ee842eb95f277b
shun20010921 <shun20010921@gmail.com>
2019-02-11 22:38:33 +0900

    #update eeprom 待機時間、WritePage,ReadPage動作確認

--- Aquila/source/eeprom.cpp
+++ Aquila/source/eeprom.cpp
@@ -11,20 +11,20 @@
 #include <util/delay.h>
 extern twi gyro;
 twi eeprom = gyro;
-#define EEPROM_ADDR 0x50
+#define EEPROM_ADDR 0xA0
 
-void erom::WriteSingle(uint16_t eeaddress,uint8_t data ) 
+void erom::WriteSingle(uint16_t eeaddress,uint8_t data ) //‚O~32767
 {
-    eeprom.Address(EEPROM_ADDR<<1,0);
+    eeprom.Address(EEPROM_ADDR,0);
     eeprom.WriteSingle(eeaddress >> 8);   // MSB
     eeprom.WriteSingle(eeaddress & 0xFF); // LSB
     eeprom.WriteSingle(data);
     eeprom.Stop();
-    _delay_ms(5);
+    _delay_ms(20);
 }
 void erom::WritePage(uint16_t eeaddress,uint8_t *data ) 
 {
-    eeprom.Address(EEPROM_ADDR<<1,0);
+    eeprom.Address(EEPROM_ADDR,0);
     eeprom.WriteSingle(eeaddress >> 8);   // MSB
     eeprom.WriteSingle(eeaddress & 0xFF); // LSB
     for (uint8_t i=0;i<64;i++){
@@ -33,16 +33,16 @@ void erom::WritePage(uint16_t eeaddress,uint8_t *data )
     }
     eeprom.WriteSingle(*data);
     eeprom.Stop();
-    _delay_ms(5);
+    _delay_ms(20);
 }
 
 uint8_t erom::ReadSingle(uint16_t eeaddress) 
 {
     uint8_t rdata = 0xFF;
-    eeprom.Address(EEPROM_ADDR<<1,0);
+    eeprom.Address(EEPROM_ADDR,0);
     eeprom.WriteSingle(eeaddress >> 8);   // MSB
     eeprom.WriteSingle(eeaddress & 0xFF); // LSB
-    eeprom.Address(0x50<<1,1); 
+    eeprom.Address(EEPROM_ADDR,1); 
 	rdata = eeprom.ReadSingle(0); //1byte–ÚŽóM
 	eeprom.Stop();
     return rdata;
@@ -50,10 +50,10 @@ uint8_t erom::ReadSingle(uint16_t eeaddress)
 
 void erom::ReadMulti(uint16_t eeaddress,uint8_t *d,uint16_t val)
 {
-	eeprom.Address(EEPROM_ADDR<<1,0);
+	eeprom.Address(EEPROM_ADDR,0);
 	eeprom.WriteSingle(eeaddress >> 8);   // MSB
 	eeprom.WriteSingle(eeaddress & 0xFF); // LSB
-	eeprom.Address(0x50<<1,1);
+	eeprom.Address(EEPROM_ADDR,1);
     for (uint16_t i=0;i<val;i++){       
         *d = gyro.ReadSingle(1); //1byte–ÚŽóM
         d++;
